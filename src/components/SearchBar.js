e870a219824f2b6c53a3595bbf4359e66f728fdd
Yordan Georgiev Yordanov <yyord@softserveinc.com>
2019-09-03 17:45:45 +0300

    comit

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+    state = { term: '' };
 
-    onInputChange(event) {
-        console.log(event.target.value)
+    onInputChange = (event) => {
+        this.setState({ term: event.target.value })
     }
 
     render() {
@@ -12,7 +13,11 @@ class SearchBar extends React.Component {
                 <form className="ui form">
                     <div className="field">
                         <label>Image Search</label>
-                        <input type="text" onChange={ this.onInputChange }/>
+                        <input 
+                            type="text"
+                            value={this.state.term}
+                            onChange={ this.onInputChange }
+                        />
                     </div>
                 </form>
             </div>
